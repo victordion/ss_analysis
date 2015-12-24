55815464daf11a29c076a6e10500e3ed79003b4d
Jianwei Cui <thucjw@gmail.com>
2015-12-23 23:58:55 -0500

    Add new funtionalities

--- analyze.py
+++ analyze.py
@@ -6,10 +6,145 @@ import matplotlib.pyplot as plt
 from world import *
 import argparse
 
-def getDateTimeFromLine(line):
+# returned value will look like "2015-12-21-00:03:11"
+def getDateTimeStringFromLogLine(line):
     tokens = line.split()
-    return tokens[0] + " " + tokens[1]
+    return tokens[0] + '|' + tokens[1]
 
+# Yeah I know it is buggy
+def correctTimeStamp(Y, M, D, h, m, s):
+    if s >= 60:
+        s = 0
+        m += 1
+    
+    if m >= 60:
+        m = 0
+        h += 1
+    
+    if h >= 24:
+        h = 0
+        D += 1
+
+    if D >= 32:
+        D = 1
+        M += 1
+
+    if M >= 13:
+        M = 1
+        Y += 1
+
+    return Y, M, D, h, m, s
+
+# return integers
+# format like "2015-12-21-00:03:11"
+def parseTimeStamp(ts_str):
+    [Y, M, D, h, m, s] = [0, 0, 0, 0, 0, 0]
+
+    if len(ts_str) >= 4:
+        Y = int(ts_str[0:4])
+
+    if len(ts_str) >= 7:
+        M = int(ts_str[5:7])
+
+    if len(ts_str) >= 10:
+        D = int(ts_str[8:10])
+
+    if len(ts_str) >= 13:
+        h = int(ts_str[11:13])
+
+    if len(ts_str) >= 16:
+        m = int(ts_str[14:16])
+
+    if len(ts_str) >= 19:
+        s = int(ts_str[17:19])
+
+    return [Y, M, D, h, m, s]
+
+# format like "2015-12-21-00:03:11"
+def getNextTimeStampByGranularity(ts_str, granularity):
+    Y, M, D, h, m, s = parseTimeStamp(ts_str)
+    
+    #print "Input: " + ts_str
+
+    if granularity == 's':
+        s += 1
+    elif granularity == 'm':
+        m += 1
+    elif granularity =='h':
+        h += 1
+    elif granularity == 'D':
+        D += 1
+    elif granularity == 'M':
+        M += 1
+    elif granulariry == 'Y':
+        Y += 1
+    else:
+        s += 1
+    
+    Y, M, D, h, m, s = correctTimeStamp(Y, M, D, h, m, s)
+    next_ts_str = "%04d-%02d-%02d|%02d:%02d:%02d" % (Y, M, D, h, m, s)
+   
+    #print "Output:" + next_ts_str
+    #raw_input()
+
+    if granularity == 's':
+        return next_ts_str[:19]
+    elif granularity == 'm':
+        return next_ts_str[:16]
+    elif granularity =='h':
+        return next_ts_str[:13]
+    elif granularity == 'D':
+        return next_ts_str[:10]
+    elif granularity == 'M':
+        return next_ts_str[:7]
+    elif granulariry == 'Y':
+        return next_ts_str[:4]
+    else:
+        return next_ts_str[:19]
+    
+
+def compareTimeStampStrings(ts1_str, ts2_str, granularity):
+    tks1 = [Y1, M1, D1, h1, m1, s1] = parseTimeStamp(ts1_str)
+    tks2 = [Y2, M2, D2, h2, m2, s2] = parseTimeStamp(ts2_str)
+    
+    if granularity == 's':
+        end_idx = 6
+    elif granularity == 'm':
+        end_idx = 5
+    elif granularity =='h':
+        end_idx = 4
+    elif granularity == 'D':
+        end_idx = 3
+    elif granularity == 'M':
+        end_idx = 2
+    elif granularity == 'Y':
+        end_idx = 1
+    else:
+        end_idx = 6
+
+    for i in range(0, end_idx):
+        if tks1[i] > tks2[i]:
+            return 1
+        elif tks1[i] < tks2[i]:
+            return -1
+
+    return 0
+
+def reduceTimeStampStringToGranularity(ts_str, granularity):
+    if granularity == 's':
+        return ts_str
+    elif granularity == 'm':
+        return ts_str[:16]
+    elif granularity =='h':
+        return ts_str[:13]
+    elif granularity == 'D':
+        return ts_str[:10]
+    elif granularity == 'M':
+        return ts_str[:7]
+    elif granularity == 'Y':
+        return ts_str[:4]
+    else:
+        return ts_str
 
 def getClientIPFromLogLine(line):
     # requesting client IP address
@@ -62,14 +197,6 @@ def ipMatched(ip, parsed_ip):
         return True
 
 
-def analyzeByClientIP(client_ip):
-    stats = getStatsByClientIP(client_ip)
-    sorted_stats = getSortedStats(stats)
-    #for e in sorted_stats:
-    #    print "    %s : %d" % e
-    plotSortedStats(sorted_stats, client_ip)
-
-
 def getStatsByClientIP(client_ip):
     stats = defaultdict(int)
     
@@ -87,6 +214,28 @@ def getStatsByClientIP(client_ip):
 def getSortedStats(stats):
     sorted_stats = sorted(stats.items(), key=operator.itemgetter(1), reverse=True)
     return sorted_stats
+
+def plotTimeSeries(stats):
+    fig = plt.figure()
+    bar_width = 1
+
+    show_len = len(stats)
+    
+    num_visits = [tp[1] for tp in stats]
+    index = range(0, show_len)
+    rects = plt.bar(index, num_visits, bar_width, color='b')
+
+    plt.xlabel("Time")
+    plt.ylabel("Number of Visits")
+    plt.title("Visiting Distribution " + stats[0][0] + "--" + stats[-1][0])
+
+    x_tick_num = 15
+    x_tick_step = max(int(len(index)/x_tick_num), 1)
+
+    plt.xticks(index[0::x_tick_step], [tp[0] for tp in stats[0::x_tick_step]], rotation=15)
+    fig.show()
+    raw_input()
+
 
 def plotSortedStats(sorted_stats, client_ip="*.*.*.*"):
     fig = plt.figure()
@@ -125,27 +274,103 @@ def showIPs():
         if "connecting" in line:
             parsed_client_ip = getClientIPFromLogLine(line)
             if parsed_client_ip not in stats_occur:
-                stats_first_connect_time[parsed_client_ip] = getDateTimeFromLine(line)
+                stats_first_connect_time[parsed_client_ip] = getDateTimeStringFromLogLine(line)
             else:
-                stats_last_connect_time[parsed_client_ip] = getDateTimeFromLine(line)
+                stats_last_connect_time[parsed_client_ip] = getDateTimeStringFromLogLine(line)
             stats_occur[parsed_client_ip] += 1
 
-    print "%15s (%10s) %6s %25s %25s" % ("Client IP", "NetName", "#", "First Appear Time", "Last Appear Time")
+    print "%15s (%15s) %6s %35s %35s" % ("Client IP", "NetName", "#", "First Appear Time", "Last Appear Time")
     for ip, occurance in stats_occur.items():
-        print "%15s (%10s) %6d %25s %25s" % (ip, getWhoisInfo(ip), occurance, stats_first_connect_time[ip], stats_last_connect_time[ip])
+        print "%15s (%15s) %6d %35s %35s" % (ip, getWhoisInfo(ip), occurance, stats_first_connect_time[ip], stats_last_connect_time[ip])
 
+
+def getStatsByTime(client_ip, granularity, start_time_str, end_time_str):
+    # A list of tuples, first element being time, second element being number of visits
+    stats = []
+    
+    last_ts_str = ""
+
+    with open("shadowsocks.log") as f:
+        content = f.readlines()
+    
+    for line in content:
+        if "connecting" in line:
+            ts_str = getDateTimeStringFromLogLine(line)
+            ts_str_reduced = reduceTimeStampStringToGranularity(ts_str, granularity)
+            
+            # if current time is earlier than needed
+            if compareTimeStampStrings(ts_str, start_time_str, granularity) == -1:
+                continue
+            # if current time is later than needed
+            elif compareTimeStampStrings(ts_str, end_time_str, granularity) == 1:
+                break
+            else:
+                if last_ts_str != ts_str_reduced:
+                    stats.append((ts_str_reduced, 1))
+                    last_ts_str = ts_str_reduced
+                else:
+                    (ts_str, num) = stats[-1]
+                    stats[-1] = (ts_str, num + 1)
+
+            #print ts_str_reduced
+    
+    stats_padded = [stats[0]]
+    for tp in stats[1:]:
+        expected_next_ts_str = getNextTimeStampByGranularity(stats_padded[-1][0], granularity)
+        while expected_next_ts_str != tp[0]:
+            stats_padded.append((expected_next_ts_str, 0))
+            expected_next_ts_str = getNextTimeStampByGranularity(stats_padded[-1][0], granularity)
+        stats_padded.append(tp)
+
+    #if isVerbose:
+    #    for tp in stats_padded:
+    #        print tp
+    return stats_padded
+
+def analyzeByClientIP(client_ip):
+    stats = getStatsByClientIP(client_ip)
+    sorted_stats = getSortedStats(stats)
+    if isVerbose:
+        for e in sorted_stats:
+            print "    %s : %d" % e
+    plotSortedStats(sorted_stats, client_ip)
+
+
+def analyzeByTime(start_time_str, end_time_str, granularity, client_ip):
+    if start_time_str is None:
+        start_time_str = "1900-01-01-01:01:01"
+    if end_time_str is None:
+        end_time_str = "2100-01-01-01:01:01"
+
+    stats = getStatsByTime(client_ip, granularity, start_time_str, end_time_str)
+    if isVerbose:
+        for e in stats:
+            print "%s --> %d" % e
+    plotTimeSeries(stats)
+
+isVerbose = False
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Analyze the log file generated by shadowsocks.')
-    parser.add_argument("verb")
+    parser.add_argument("verb", help="verb can be [show_ips] or [get_stats_by_ip] or [get_stats_by_time]")
     parser.add_argument("--client_ip", help="specify client ip")
+    parser.add_argument("-s", "--start_time", help="specify start time")
+    parser.add_argument("-e", "--end_time", help="specify end time")
+    parser.add_argument("-g", "--granularity", help="granularity used in time analysis, can be [m], [h], [d]")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Verbose mode")
 
 
     args = parser.parse_args()
+
+    if args.verbose:
+        isVerbose = True
+
     if args.verb == "show_ips":
         showIPs()
-    elif args.verb == "get_stats":
+    elif args.verb == "get_stats_by_ip":
         analyzeByClientIP(args.client_ip)
+    elif args.verb == "get_stats_by_time":
+        analyzeByTime(args.start_time, args.end_time, args.granularity, args.client_ip)
     else:
         print "Valid positional argument!!!"
 
